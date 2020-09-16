c3f6b6faeed97828d8e388b0b5647a8ff32c9ffe
NateOwl <natedog112004@gmail.com>
2020-09-16 05:40:26 +0000

    Matrix function completed

--- src/matrix.py
+++ src/matrix.py
@@ -1,38 +1,131 @@
 class Matrix():
-  def __init__(self, matrix_values):
-    self.elements=matrix_values
+    def __init__(self, matrix_values):
+        self.elements = matrix_values
+        self.num_rows = len(matrix_values)
+        self.num_cols = len(matrix_values[0])
+       
+        
 
-  def copy(self):
-    return Matrix(self.elements)
-  
-  def add(self,value):
-      new_matrix=[[0,0],[0,0]]
-      for i in range(2): 
-        for j in range(2): 
-          new_matrix[i][j] = self.elements[i][j] + value.elements[i][j]
-          
-      return Matrix(new_matrix)
-  
-  def subtract(self,values):
-    new_matrix=[[0,0],[0,0]]
-    for i in range(2): 
-        for j in range(2): 
-            new_matrix[i][j] = self.elements[i][j] - values.elements[i][j]
-    return Matrix(new_matrix)
+    def copy(self):
+        return Matrix(self.elements)
 
-  def scalar_multiply(self,scalar):
-    new_matrix=[[0,0],[0,0]]
-    for i in range(len(self.elements[0])): 
-        for j in range(len(self.elements[1])): 
-            new_matrix[i][j] =  self.elements[i][j] * scalar
-    return Matrix(new_matrix)
+    def add(self, matrix):
+        new_matrix = [[0 for _ in range(self.num_cols)] for _ in range(self.num_rows)] 
+        for row in range(self.num_rows): 
+            for col in range(self.num_cols ): 
+              sum_of_elements= self.elements[row][col] + matrix.elements[row][col]
+              new_matrix[row][col] = sum_of_elements
+
+        return Matrix(new_matrix)
+  
+    def subtract(self, matrix):
+        new_matrix = [[0 for _ in range(self.num_cols)] for _ in range(self.num_rows)] 
+        for i in range(self.num_rows): 
+            for j in range(self.num_cols): 
+                sum_of_elements = self.elements[i][j] - matrix.elements[i][j]
+                new_matrix[i][j] = sum_of_elements
+        return Matrix(new_matrix)
+
+    def scalar_multiply(self, scalar):
+        scalar = 1 / scalar
+        new_matrix = [[0 for _ in range(self.num_cols)] for _ in range(self.num_rows)] 
+        for row_index in range(len(self.elements)): 
+            for col_index in range(len(self.elements[0])): 
+
+              new_matrix[row_index][col_index] = self.elements[row_index][col_index] / scalar
+
+        return Matrix(new_matrix)
     
+    def matrix_multiply(self,matrix):
+        new_matrix = [[0 for _ in range(self.num_rows)] for _ in range(matrix.num_cols)] 
+        for row_index in range(self.num_rows): 
+          for col_index in range(matrix.num_cols): 
+            row=self.elements[row_index]
+            col=[matrix.elements[k][col_index] for k in range(self.num_cols)]
 
-  def matrix_multiply(self,matrix):
-    new_matrix=[[0,0],[0,0]]
-    for i in range(len(self.elements[0])): 
-        for j in range(len(matrix.elements[0])): 
-          for k in range(len(matrix.elements[1])):  
-            new_matrix[i][j] += self.elements[i][k] * matrix.elements[k][j]
-    return Matrix(new_matrix)
+            dot_product = 0
+            for i in range(len(row)):
+                dot_product += row[i] * col[i]
+            new_matrix[row_index][col_index] = dot_product
+
+        return Matrix(new_matrix)
+
+    def transpose(self):
+        new_matrix = [[0 for _ in range(self.num_rows)] for _ in range(self.num_cols)] 
+        for row_index in range(self.num_rows): 
+          for col_index in range(self.num_cols): 
+            element = self.elements[row_index][col_index]  
+            new_matrix[col_index][row_index] = element 
+  
+        return Matrix(new_matrix)
+
+    def is_equal(self,matrix):
+      if self.num_cols != matrix.num_cols:
+        return False
+      else: 
+        if self.num_rows != matrix.num_rows:
+          return False
+        else:
+          for i in range(len(self.elements)): 
+            for j in range(len(self.elements[0])):
+              if self.elements[i][j] != matrix.elements[i][j]:
+                return False
+      return True
+    
+    def get_pivot_row(self, column_index):
+      for row in range(self.num_rows):
+        for value in range(column_index):
+          if(self.elements[row][value] != 0):
+            row += 1
+        if(self.elements[row][column_index] != 0):
+          return row
+      return 'None'
+# see if all entries to left of index are zero
+    def swap_rows(self, row_index1, row_index2):
+      self.elements[row_index1], self.elements[row_index2] = self.elements[row_index2], self.elements[row_index1]
+      return self.elements
+    
+    def normalize_row(self, row_index):
+      non_zero=[]
+      for value in range(self.num_cols):
+        if(self.elements[row_index][value] != 0):
+           non_zero.append(self.elements[row_index][value])
+      diviser = non_zero[0]
+      for col in range(self.num_cols):
+        self.elements[row_index][col] = int(self.elements[row_index][col] / diviser)
+      return self.elements
+
+    def first_non_zero_element_index(self,row_index):
+      row = self.elements[row_index]
+      first_non_zero_index = 0
+      for row_element in row:
+        if row_element == 0:
+          first_non_zero_index += 1
+        else:
+          break
+      return first_non_zero_index
+
+    def row_subtract(self,list_1, multiplier, list_2):
+      result = []
+      for i in range(len(list_1)):
+        result.append(list_1[i] - multiplier * list_2[i])
+      return result
+
+    def clear_below(self, row_index): 
+      pivot_index = self.first_non_zero_element_index(row_index)
+      row = self.elements[row_index]
+      for row_below_index in range(row_index + 1, self.num_rows):
+        row_below = self.elements[row_below_index]
+        multiplier = row_below[pivot_index] / row[pivot_index]
+        self.elements[row_below_index] = self.row_subtract(row_below, multiplier, row)
+      return self.elements
+    
+    def clear_above(self, row_index):
+      pivot_index = self.first_non_zero_element_index(row_index)
+      row = self.elements[row_index]
+      for row_above_index in range(row_index):
+        row_above = self.elements[row_above_index]
+        multiplier = row_above[pivot_index] / row[pivot_index]
+        self.elements[row_above_index] = self.row_subtract(row_above, multiplier, row)
+      return self.elements
 
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,46 +1,179 @@
 import sys
 sys.path.append('src')
 from matrix import Matrix
-A = Matrix([[1,3],[2,4]])
-print(A.elements)
-assert A.elements == [[1,3],[2,4]], 'Answer is supposed to be [[1,3],[2,4]]'
-print('Passed')
 
-B = A.copy()
-A = 'resetting A to a string'
-print('Testing method copy')
-print(B.elements)
-assert B.elements == [[1,3],[2,4]], 'Answer is supposed to be [[1,3],[2,4]]'
-print('Passed')
+tests_1 = True 
+if tests_1 == True: 
+  print('testing matrix')
+  A = Matrix([[1,3],[2,4]])
+  print(A.elements)
+  assert A.elements == [[1,3],[2,4]], 'Answer is supposed to be [[1,3],[2,4]]'
+  print('Passed')
 
+  B = A.copy()
+  A = 'resetting A to a string'
+  print('Testing method copy')
+  print(B.elements)
+  assert B.elements == [[1,3],[2,4]], 'Answer is supposed to be [[1,3],[2,4]]'
+  print('Passed')
 
-C = Matrix([[1,0],[2,-1]])
-print(C.elements)
-D = B.add(C)
+  print('Testing method add')
+  C = Matrix([[1,0],[2,-1]])
+  D = B.add(C)
+  print(D.elements)
+  assert D.elements == [[2,3],[4,3]], 'Answer is supposed to be [[2,3],[4,3]]'
+  print('Passed')
+  #[[2,3],[4,3]]
 
-print('Testing method add')
-print(D.elements)
-assert D.elements == [[2,3],[4,3]], 'Answer is supposed to be [[2,3],[4,3]]'
-print('Passed')
-#[[2,3],[4,3]]
+  print('Testing method subtract')
+  E = B.subtract(C)
+  print(E.elements)
+  assert E.elements == [[0,3],[0,5]], 'Answer is supposed to be [[0,3],[0,5]]'
+  print('Passed')
+  #[[0,3],[0,5]]
 
-E = B.subtract(C)
-print('Testing method subtract')
-print(E.elements)
-assert E.elements == [[0,3],[0,5]], 'Answer is supposed to be [[0,3],[0,5]]'
-print('Passed')
-#[[0,3],[0,5]]
+  print('Testing method scalar_multiply')
+  F = B.scalar_multiply(2)
+  print(F.elements)
+  assert F.elements == [[2,6],[4,8]], 'Answer is supposed to be [[2,6],[4,8]]'
+  print('Passed')
+  #[[2,6],[4,8]]
+  
+  G = B.matrix_multiply(C)
+  print('Testing method matrix_multiply')
+  print(G.elements)
+  assert G.elements == [[7,-3],[10,-4]], 'Answer is supposed  to be [[7,-3],[10,-4]]'
+  print('Passed')
+  #[[7,-3],[10,-4]]
 
-F = B.scalar_multiply(2)
-print('Testing method subtract')
-print(F.elements)
-assert F.elements == [[2,6],[4,8]], 'Answer is supposed to be [[2,6],[4,8]]'
-print('Passed')
-#[[2,6],[4,8]]
+tests_2 = True
+if tests_2 == True:
+  A = Matrix([[1,0,2,0,3],
+                [0,4,0,5,0],
+                [6,0,7,0,8],
+                [-1,-2,-3,-4,-5]])
+  print(A.num_rows, A.num_cols)
+  (4, 5)
+  
+  print('testing transpose')
+  A_t = A.transpose()
+  print(A_t.elements)
+  assert A_t.elements == [[ 1,  0,  6, -1],[ 0,  4,  0, -2],[ 2,  0,  7, -3],[ 0,  5,  0, -4],[ 3,  0,  8, -5]],"Correct answer was [[ 1,  0,  6, -1],[ 0,  4,  0, -2],[ 2,  0,  7, -3],[ 0,  5,  0, -4],[ 3,  0,  8, -5]]"
+  print('Passed')
 
-G = B.matrix_multiply(C)
-print('Testing method subtract')
-print(G.elements)
-assert G.elements == [[7,-3],[10,-4]], 'Answer is supposed to be [[7,-3],[10,-4]]'
-print('Passed')
-#[[7,-3],[10,-4]]
+  print('testing matrix_multiply')
+  B = A_t.matrix_multiply(A)
+  print(B.elements)
+  assert B.elements ==[[38, 2, 47, 4, 56], [2, 20, 6, 28, 10],[47,6,62,12,77], [4, 28, 12, 41, 20], [56, 10, 77, 20,98]]
+  print('passed')
+
+  print('testing scalar_multiply')
+  C = B.scalar_multiply(0.1)
+  print(C.elements)
+  assert C.elements == [[3.8,  .2, 4.7,  .4, 5.6],
+  [ .2, 2.0,  .6, 2.8, 1.0],
+  [4.7,  .6, 6.2, 1.2, 7.7],
+  [ .4, 2.8, 1.2, 4.1, 2.0],
+  [5.6, 1.0, 7.7, 2.0, 9.8]]
+  print('passed')
+
+  print('testing subtract')
+  D = B.subtract(C)
+  print(D.elements)
+  assert D.elements == [[34.2,  1.8, 42.3,  3.6, 50.4],
+  [ 1.8, 18. ,  5.4, 25.2,  9. ],
+  [42.3,  5.4, 55.8, 10.8, 69.3],
+  [ 3.6, 25.2, 10.8, 36.9, 18. ],
+  [50.4,  9. , 69.3, 18. , 88.2]]
+  print('passed')
+  
+  print('testing add')
+  E = D.add(C)
+  print(E.elements)
+  assert E.elements == [[38,  2, 47,  4, 56],
+  [ 2, 20,  6, 28, 10],
+  [47,  6, 62, 12, 77],
+  [ 4, 28, 12, 41, 20],
+  [56, 10, 77, 20, 98]]
+  print('passed')
+
+  print('testing is_equal') 
+  print((E.is_equal(B), E.is_equal(C)))
+  assert (E.is_equal(B), E.is_equal(C)) == (True, False)
+  print('passed')
+
+test__3 = True
+if test__3 == True:
+  A = Matrix( [[0, 1, 2],
+              [3, 6, 9],
+              [2, 6, 8]])
+  print('testing get_pivot_row')
+  print(A.get_pivot_row(0))
+  assert A.get_pivot_row(0) == 1, 'Answer wasnt 1'
+  print('passed')
+  1
+  print('testing swap_rows')
+  A.swap_rows(0,1)
+  print(A.elements)
+  assert A.elements == [[3, 6, 9],
+  [0, 1, 2],
+  [2, 6, 8]], 'Answer wasnt [[3, 6, 9],[0, 1, 2],[2, 6, 8]]'
+  
+  print('testing normalize_row')
+  A.normalize_row(0)
+  print(A.elements)
+  assert A.elements == [[1, 2, 3],
+  [0, 1, 2],
+  [2, 6, 8]], 'Answer wasnt [[1, 2, 3],[0, 1, 2],[2, 6, 8]]'
+  print('passed')
+
+  print('testing clear_below')
+  A.clear_below(0)
+  print(A.elements)
+  assert A.elements == [[1, 2, 3], [0, 1, 2], [0, 2, 2]]
+  print('passed')
+
+  print('testing get_pivot_row')
+  print(A.get_pivot_row(1))
+  assert A.get_pivot_row(1) == 1, 'Answer wasnt 1'
+  print('passed')
+  1
+  print('testing normalize_row')
+  A.normalize_row(1)
+  print(A.elements)
+  assert A.elements == [[1, 2, 3],
+  [0, 1, 2],
+  [0, 2, 2]]
+  print ('passed')
+  
+  print('testing clear_below')
+  A.clear_below(1)
+  print(A.elements)
+  assert A.elements == [[1, 2, 3],[0, 1, 2],[0, 0, -2]]
+  print('passed')
+
+  
+  print('testing get_pivot_row')
+  print(A.get_pivot_row(2))
+  assert A.get_pivot_row(2) == 2, 'Answer wasnt 2'
+  print('passed')
+  
+  print('testing normalize_row')
+  A.normalize_row(2)
+  print(A.elements)
+  assert A.elements == [[1, 2, 3],
+  [0, 1, 2],
+  [0, 0, 1]]
+  print('passed')
+  
+  print('testing clear_above')
+  A.clear_above(2)
+  print(A.elements)
+  assert A.elements == [[1, 2, 0],[0, 1, 0],[0, 0, 1]]
+  print('Passed')
+
+  print('testing clear_above')
+  A.clear_above(1)
+  print(A.elements)
+  assert A.elements == [[1, 0, 0],[0, 1, 0],[0, 0, 1]]
+  print('passed')
